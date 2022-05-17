c661a885068a012de6c33abe0f1011f24f49ec16
Jinseok Heo <js.heo7747@gmail.com>
2022-05-17 09:02:16 +0900

    Feat: so_long finished

--- so_long/srcs/exit.c
+++ so_long/srcs/exit.c
@@ -6,7 +6,7 @@
 /*   By: jiheo <jiheo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 08:30:57 by jiheo             #+#    #+#             */
-/*   Updated: 2022/03/30 08:36:11 by jiheo            ###   ########.fr       */
+/*   Updated: 2022/05/16 12:04:10 by jiheo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,12 @@ bool	is_done(t_info *info)
 {
 	int	i;
 	int	j;
+    const t_coordinate p_coord = info->player.coord;
 
 	i = -1;
 	j = -1;
+    if (info->map->map[p_coord.row][p_coord.col] != 'E')
+        return (false);
 	while (++i < info->map->height)
 	{
 		while (++j < info->map->width)
